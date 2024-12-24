0f536616885c4d763ed9443e8832e82c3d9be320
abhisheksharma <sharma.abhi3199@gmail.com>
2024-12-24 20:34:08 +0530

    calendar updated with prev and next

--- js/calenar-new/main.js
+++ js/calenar-new/main.js
@@ -15,7 +15,10 @@ let year = date.getFullYear();
 let months =["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
 
 console.log(months[month])
+const days = document.querySelector('.days');
 
+const m = document.querySelector('.month-year > span')
+const y = document.querySelector('.month-year > span + span')
 
 function displayCalendar(){
 
@@ -31,6 +34,40 @@ function displayCalendar(){
     let DaysPrevMonth = new Date(year,month,0).getDate()
     console.log(DaysPrevMonth)
 
+    let u = ""
+        // write your code for previous days to show
+        for(let i=0;i<firstDay;i++){
+           u +=  `<li class="dis" > ${DaysPrevMonth - (firstDay-1)+i}</li>`
+        }
+
+        // current days display on calendar
+        for(let i=1;i<=DaysCurrMonth;i++){
+            u +=  `<li class="en"> ${i}</li>`
+         }
+
+         // write a logic for next month days; 
+
+    m.innerHTML = months[month];
+    y.innerHTML = year;
+
+    days.innerHTML = u;
+
+    
+    // 0-6
+    // previous month's days = 30
+    
+    // i = 4 // from this index we are going to fill items 
+    // i = 0,1,2,3
+    //     27 28 29 30
+
+    // function displayPrevDays(index,month){
+    //     // write your code here
+    //     for(let i=0;i<index;i++){
+    //         console.log(month - (index-1)+i)
+    //     }
+    // }
+    // displayPrevDays(4,30)
+
     // display previous month days 
     // 30 ,firstDay-1 -> 28+1 
     // loop to run firstDay (n)
@@ -45,14 +82,41 @@ function displayCalendar(){
 displayCalendar()
 
 // event listener that will increase or decrease the month varible 
+const prev = document.querySelector('.action-buttons > button')
+const next = document.querySelector('.action-buttons > button + button')
 
-if(month > 11) {
-    year++;
-}
-if(month < 0) {
-    year--;
-}
-date = new(year,month,1)
+console.log(next)
+prev.addEventListener('click',()=>{
+
+    month--; // 0-11
+    if(month < 0){
+        date = new Date(year,month,1)
+        year = date.getFullYear();
+        month = date.getMonth();
+    }
+
+    displayCalendar()
+
+
+})
+next.addEventListener('click',()=>{
+
+    month++;
+
+    if(month > 11){
+        date = new Date(year,month,1)
+        year = date.getFullYear();
+        month = date.getMonth();
+    }
+    displayCalendar()
+})
+// if(month > 11) {
+//     year++;
+// }
+// if(month < 0) {
+//     year--;
+// }
+// date = new(year,month,1)
 
 
 
